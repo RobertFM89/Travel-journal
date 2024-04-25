dbaeb73626a4dfbe89aad1c0215579082758aa52
Robert Muresan <robotzelu89@gmail.com>
2024-04-25 01:13:57 +0100

    add database and more

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 
 const app = express();
+const PORT = process.env.PORT || 3000
 
-app.listen()
+app.listen(PORT, () => console.log( `Servidor corriendo en http://localhost:${PORT}`))
